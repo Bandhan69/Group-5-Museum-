954dfbe1450148456141f67163247714e18de092
macbookair <bandhankarmakar469@gmail.com>
2025-08-12 09:02:30 +0600

    All Group member name updated

---
+++ src/main/java/com/oop/groupfive/summer25_section1/Bandhan/BkDashboardController.java
@@ -0,0 +1,7 @@
+package com.oop.groupfive.summer25_section1.Bandhan;
+
+public class BkDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/oop/groupfive/summer25_section1/Bandhan/UserProfile.java
@@ -0,0 +1,7 @@
+package com.oop.groupfive.summer25_section1.Bandhan;
+
+public class UserProfile
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
