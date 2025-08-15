0d972a9cc6a89a66579dceeeadedc44a0e5dabb0
macbookair <bandhankarmakar469@gmail.com>
2025-08-15 13:48:01 +0600

    bKDashboard.fxml and userProfile are deleted

--- src/main/java/com/oop/groupfive/summer25_section1/bandhan/BkDashboardController.java
+++
@@ -1,7 +0,0 @@
-package com.oop.groupfive.summer25_section1.bandhan;
-
-public class BkDashboardController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- src/main/java/com/oop/groupfive/summer25_section1/bandhan/UserProfile.java
+++
@@ -1,7 +0,0 @@
-package com.oop.groupfive.summer25_section1.bandhan;
-
-public class UserProfile
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
