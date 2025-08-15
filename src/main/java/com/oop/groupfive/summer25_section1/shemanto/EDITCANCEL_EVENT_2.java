40f052c1a1eb2b16223270f8147e95949e139899
ShemantoK <Shemantokhan9@gmail.com>
2025-08-15 23:29:59 +0600

    commit

---
+++ src/main/java/com/oop/groupfive/summer25_section1/shemanto/EDITCANCEL_EVENT_2.java
@@ -0,0 +1,27 @@
+package com.oop.groupfive.summer25_section1.shemanto;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class EDITCANCEL_EVENT_2 {
+    @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private TableColumn venueCol;
+    @javafx.fxml.FXML
+    private TableColumn titleCol;
+
+    @javafx.fxml.FXML
+    public void refreshlistOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelselectedOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void editselectedOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/oop/groupfive/summer25_section1/shemanto/View_Event_Proposals_3.java
@@ -0,0 +1,16 @@
+package com.oop.groupfive.summer25_section1.shemanto;
+
+
+import javafx.scene.control.TableColumn;
+
+public class View_Event_Proposals_3 {
+
+    @javafx.fxml.FXML
+    private TableColumn proposalstatusCol;
+    @javafx.fxml.FXML
+    private TableColumn proposalsubmitedbyCol;
+    @javafx.fxml.FXML
+    private TableColumn proposaldateCol;
+    @javafx.fxml.FXML
+    private TableColumn proposaltitleCol;
+}
